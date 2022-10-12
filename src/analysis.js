c20b4ac3e34b9b94a6b4d8bb1bbb9dc92d5fc01b
mac <mac@chidinmas-MacBook-Air.local>
2022-10-12 13:26:34 +0100

    task 3

--- src/analysis.js
+++ src/analysis.js
@@ -1,13 +1,121 @@
-const { getTrips } = require('api');
-
+const { getTrips, getDriver } = require("api");
 /**
  * This function should return the trip data analysis
  *
  * Question 3
  * @returns {any} Trip data analysis
  */
-async function analysis() {
-  // Your code goes here
-}
 
+ async function analysis() {
+  try {
+    const trips = await getTrips();
+    const newTrips = [...trips];
+    const totalAmount = newTrips.reduce((acc, cur) => {
+      acc += +cur.billedAmount || +cur.billedAmount.split(",").join("");
+      return acc;
+    }, 0);
+
+    let testObj = {
+      noOfCashTrips: 0,
+      noOfNonCashTrips: 0,
+      cashBilledTotal: 0,
+      nonCashBilledTotal: 0,
+    };
+    const totalCashTrips = newTrips.reduce((acc, cur) => {
+      if (cur.isCash === true) {
+        testObj.noOfCashTrips += 1;
+        testObj.cashBilledTotal +=
+          +cur.billedAmount || +cur.billedAmount.split(",").join("");
+      } else {
+        testObj.noOfNonCashTrips += 1;
+        testObj.nonCashBilledTotal +=
+          +cur.billedAmount || +cur.billedAmount.split(",").join("");
+      }
+      return testObj;
+    }, testObj);
+
+    const driverIds = newTrips.map((el) => el.driverID);
+    const uniqueDrivers = [...new Set(driverIds)];
+    const details = uniqueDrivers.map((el) => getDriver(el));
+    const final = await Promise.allSettled(details);
+
+    let driversWithMoreCars = 0;
+    for (let i = 0; i < final.length; i++) {
+      if (final[i].status === "rejected") {
+        continue;
+      } else if (final[i].value.vehicleID.length > 1) {
+        driversWithMoreCars++;
+      }
+    }
+    const mostTrips = {};
+    let driversWithMoreTrips = [];
+    for (let i = 0; i < driverIds.length; i++) {
+      mostTrips[driverIds[i]] = (mostTrips[driverIds[i]] || 0) + 1;
+    }
+    let values = Object.values(mostTrips);
+    let max = Math.max(...values);
+    for (key in mostTrips) {
+      if (mostTrips[key] == max) {
+        driversWithMoreTrips.push(key);
+      }
+    }
+    let mostTripsByDriver = {};
+    let res = await getDriver(driversWithMoreTrips[0]);
+    mostTripsByDriver.name = res.name;
+    mostTripsByDriver.email = res.email;
+    mostTripsByDriver.phone = res.phone;
+    mostTripsByDriver.noOfTrips = 0;
+    mostTripsByDriver.totalAmountEarned = 0;
+    for (let i = 0; i < newTrips.length; i++) {
+      if (newTrips[i].driverID == driversWithMoreTrips[0]) {
+        mostTripsByDriver.totalAmountEarned +=
+          +newTrips[i].billedAmount ||
+          +newTrips[i].billedAmount.split(",").join("");
+        mostTripsByDriver.noOfTrips += 1;
+      }
+    }
+
+    let obj2 = {};
+    obj2[driversWithMoreTrips[0]] = 0;
+    obj2[driversWithMoreTrips[1]] = 0;
+
+    const objKeys = Object.keys(obj2);
+    newTrips.forEach((trip) => {
+      if (trip.driverID === objKeys[0]) {
+        obj2[driversWithMoreTrips[0]] +=
+          +trip.billedAmount || +trip.billedAmount.split(",").join("");
+      } else if (trip.driverID === objKeys[1]) {
+        obj2[driversWithMoreTrips[1]] +=
+          +trip.billedAmount || +trip.billedAmount.split(",").join("");
+      }
+    });
+    let maximum = 0;
+    for (key in obj2) {
+      if (obj2[`${key}`] > 0) {
+        maximum = key;
+      }
+    }
+    const highestEarning = await getDriver(maximum);
+    const result = {
+      noOfCashTrips: testObj.noOfCashTrips,
+      noOfNonCashTrips: testObj.noOfNonCashTrips,
+      billedTotal: Number(totalAmount.toFixed(2)),
+      cashBilledTotal: testObj.cashBilledTotal,
+      nonCashBilledTotal: Number(testObj.nonCashBilledTotal.toFixed(2)),
+      noOfDriversWithMoreThanOneVehicle: driversWithMoreCars,
+      mostTripsByDriver: mostTripsByDriver,
+      highestEarningDriver: {
+        name: highestEarning.name,
+        email: highestEarning.email,
+        phone: highestEarning.phone,
+        noOfTrips: mostTripsByDriver.noOfTrips,
+        totalAmountEarned: obj2[maximum],
+      },
+    };
+    return result
+  } catch (e) {
+    console.log(e);
+  }
+}
+// console.log(analysis());
 module.exports = analysis;
--- src/report.js
+++ src/report.js
@@ -1,14 +1,114 @@
 const { getTrips } = require('api');
+const { getDriver } = require('api');
+const { getVehicle } = require('api');
 
-/**
- * This function should return the data for drivers in the specified format
- *
- * Question 4
- *
- * @returns {any} Driver report data
- */
 async function driverReport() {
-  // Your code goes here
-}
+  const allTrips = await getTrips()
+  let totalDriversData = {};
+  for (let i = 0; i < allTrips.length; i++) {
+    try {
+      const driver = await getDriver(allTrips[i].driverID);
+      totalDriversData[allTrips[i].driverID] = driver;
+      totalDriversData[allTrips[i].driverID]["vehicle"] = [];
 
+      if (driver.vehicleID.length > 1) {
+        for (let m = 0; m < driver.vehicleID.length; m++) {
+          const vehicle = await getVehicle(driver.vehicleID[j]);
+          totalDriversData[allTrips[i].driverID]["vehicle"].push(vehicle);
+        }
+      } else {
+        const vehicle = await getVehicle(driver.vehicleID);
+        totalDriversData[allTrips[i].driverID]["vehicle"].push(vehicle);
+      }
+    } catch (error) {
+      continue;
+    }
+  }
+  const totalOutput = []
+  let objData = {
+    fullname: '',
+    phone: '',
+    id: '',
+    vehicles: [],
+    noOfTrips: 0,
+    noOfCashTrips: 0,
+    noOfNonCashTrips: 0,
+    trips: [],
+    totalAmountEarned: 0,
+    totalCashAmount: 0,
+    totalNonCashAmount: 0
+  }
+  let userVehicle = {
+    plate: '',
+    manufacturer: '',
+  }
+  let userTrip = {
+    user: '',
+    created: '',
+    pickup: '',
+    destination: '',
+    billed: 0,
+    isCash: false
+  }
+
+  const copyOfobjData = JSON.parse(JSON.stringify(objData));
+  const copyOfVehicles = JSON.parse(JSON.stringify(userVehicle));
+  const copyOfTrips = JSON.parse(JSON.stringify(userTrip));
+
+  for (const key in allTrips) {
+    for (let i = 0; i < allTrips.length; i++) {
+      if (key === allTrips[i].driverID) {
+        objData.totalAmountEarned +=
+          +(allTrips[i].billedAmount) ||
+          +(allTrips[i].billedAmount.split(",").join(""));
+        objData.noOfTrips++;
+        objData.id = key;
+        objData.fullname = allTrips[key].name;
+        objData.phone = allTrips[key].phone;
+        if (allTrips[i].isCash === true) {
+          objData.totalCashAmount +=
+            +(allTrips[i].billedAmount) ||
+            +(allTrips[i].billedAmount.split(",").join(""));
+          objData.noOfCashTrips++;
+        } else {
+          objData.totalNonCashAmount +=
+            +(allTrips[i].billedAmount) ||
+            +(allTrips[i].billedAmount.split(",").join(""));
+          objData.noOfNonCashTrips++;
+        }
+        userTrip.user = allTrips[i].user.name;
+        userTrip.created = allTrips[i].created;
+        userTrip.pickup = allTrips[i].pickup.address;
+        userTrip.destination = allTrips[i].destination.address;
+        userTrip.isCash = allTrips[i].isCash;
+        userTrip.billed =
+          +(allTrips[i].billedAmount) ||
+          +(allTrips[i].billedAmount.split(",").join(""));
+
+        for (let j = 0; j < allTrips[key].vehicle.length; j++) {
+          userVehicle.plate = allTrips[key].vehicle[j].plate;
+          userVehicle.manufacturer = allTrips[key].vehicle[j].manufacturer;
+          if (objData.vehicles.length < allTrips[key].vehicle.length) {
+            objData.vehicles.push(userVehicle);
+            userVehicle = JSON.parse(JSON.stringify(copyOfTrips));
+          }
+        }
+        objData.trips.push(objData);
+        userTrip = JSON.parse(JSON.stringify(copyOfTrips));
+      }
+    }
+    objData.totalAmountEarned = Number(
+      Math.round(objData.totalAmountEarned * 100) / 100
+    );
+    objData.totalCashAmount = Number(objData.totalCashAmount * 100) / 100;
+    objData.totalNonCashAmount = Number(
+      Math.round(objData.totalNonCashAmount * 100) / 100
+    );
+    totalOutput.push(objData);
+    objData = JSON.parse(JSON.stringify(copyOfobjData));
+  }
+  // console.log(modelOutput);
+  return totalOutput
+}
+driverReport()
 module.exports = driverReport;
